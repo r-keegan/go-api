16779c56715e03bb6c57bb0e51aed47d92609b8e
rebeccakeegan <rebeccachristinekeegan@gmail.com>
2020-03-06 11:41:51 +1000

    create test to check response when first adding a product then deleting the same product

--- main_test.go
+++ main_test.go
@@ -170,3 +170,21 @@ func TestUpdateProduct(t *testing.T) {
 			originalProduct["price"], m["price"], m["price"])
 	}
 }
+
+func TestDeleteProduct(t *testing.T) {
+	clearTable()
+	addProducts(1)
+
+	req, _ := http.NewRequest("GET", "/product/1", nil)
+	response := executeRequest(req)
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("DELETE", "/product/1", nil)
+	response = executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+
+	req, _ = http.NewRequest("GET", "/product/1", nil)
+	response = executeRequest(req)
+	checkResponseCode(t, http.StatusNotFound, response.Code)
+}
