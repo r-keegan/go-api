b749774857a96cb0c89936117253014b9264af15
rebeccakeegan <rebeccachristinekeegan@gmail.com>
2020-03-06 10:57:06 +1000

    create test to check response when fetching a product

--- main_test.go
+++ main_test.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"os"
+	"strconv"
 	"testing"
 )
 
@@ -109,5 +110,27 @@ func TestCreateProduct(t *testing.T) {
 	if m["id"] != 1.0 {
 		t.Errorf("Exepected product ID to be '1'. Got '%v'",
 			m["id"])
+	}
+}
+
+func TestGetProduct(t *testing.T) {
+	clearTable()
+
+	addProducts(1)
+
+	req, _ := http.NewRequest("GET", "/product/1", nil)
+	response := executeRequest(req)
+
+	checkResponseCode(t, http.StatusOK, response.Code)
+}
+
+func addProducts(count int) {
+	if count < 1 {
+		count = 1
+	}
+
+	for i := 0; i < count; i++ {
+		a.DB.Exec("INSERT INTO products(name, price) VALUES(£1, £2)",
+			"Product "+strconv.Itoa(i), (i+1.0)*10)
 	}
 }
